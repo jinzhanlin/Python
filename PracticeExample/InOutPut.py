0130642a8fff23d492ddaad7acc5253de6e4ca9f
jinzhanlin <linm123yy@gmail.com>
2018-12-23 16:41:19 +0800

    [Python Practice] Print and Input String
    1. Print Stirng
    2. Input String then Printf String

---
+++ PracticeExample/InOutPut.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-'
+
+""" 等同 /**/
+
+Spyder Editor
+
+This is a temporary script file.
+
+""" 
+
+# = 註解
+
+#  print() 输出
+#  print()会依次打印每个字符串，遇到逗号“,”会输出一个空格
+
+print('\n========  Print Example =========================')
+
+print("Hello Anaconda")
+
+print('The quick brown fox', 'jumps over', 'the lazy dog')
+
+print('Hello','\nWorld')
+
+print('100 + 200 =', 100 + 200) 
+
+print('') # 空一行 = \n
+
+
+print('\n=========  Input Example  ========================')
+
+#  input() 输入
+
+print('Enter Name :')
+name = input()
+print('Input Name =',name)
+
