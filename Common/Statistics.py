617d2348e332cae29777eb508b4772571751e3e7
jinzhanlin <linm123yy@gmail.com>
2019-03-24 16:22:33 +0800

    [Statistics] 移除行尾空格

--- Common/Statistics.py
+++ Common/Statistics.py
@@ -21,50 +21,50 @@ import os.path
 
 """========== Find Data Times in Array  ============================="""
 def DataValueCounts(DtaNPArray, SetPrintResult): # 計算元素出現次數
-    
+
     ValueCnts = pd.value_counts(DtaNPArray)  # 對表格中每个值进行计数并且排序
-    
+
     if SetPrintResult == True:
-        print("\n -----Value Counts，對表格中每個值排序並計數 -----\n", ValueCnts)        
-    
+        print("\n -----Value Counts，對表格中每個值排序並計數 -----\n", ValueCnts)
+
     return ValueCnts
 
 """========== Basic Statistic - Find Max Min Mean SD Var  ============================="""
 def BasicStatistic(DtaNPArray, SetPrintResult): # 計算元素出現次數
-    
 
-    MaxValue = np.max(DtaNPArray) 
 
-    MinValue = np.min(DtaNPArray) 
-    
+    MaxValue = np.max(DtaNPArray)
+
+    MinValue = np.min(DtaNPArray)
+
     Mean = np.mean(DtaNPArray)
 
     SD = np.std(DtaNPArray) # 标准差
 
     Var = np.var(DtaNPArray) # Var
-    
+
     if SetPrintResult == 1: # Print All Result
-        print("\n -----Max -----\n", MaxValue) 
-        print("\n -----Min -----\n", MinValue) 
-        print("\n -----Mean -----\n", Mean) 
-        print("\n -----SD  -----\n", SD) 
-        print("\n -----Var -----\n", Var) 
-        
+        print("\n -----Max -----\n", MaxValue)
+        print("\n -----Min -----\n", MinValue)
+        print("\n -----Mean -----\n", Mean)
+        print("\n -----SD  -----\n", SD)
+        print("\n -----Var -----\n", Var)
+
     elif SetPrintResult == 2: # Print Max
         print("\n -----Max -----\n", MaxValue)
-        
+
     elif SetPrintResult == 3: # Print Min
         print("\n -----Min -----\n", MinValue)
-    
+
     elif SetPrintResult == 4: # Print Mean
         print("\n -----Mean -----\n", Mean)
-    
+
     elif SetPrintResult == 5: # Print SD
-        print("\n -----SD  -----\n", SD) 
-    
+        print("\n -----SD  -----\n", SD)
+
     elif SetPrintResult == 6: # Print Var
-        print("\n -----Var -----\n", Var) 
-        
-        
+        print("\n -----Var -----\n", Var)
+
+
     return MaxValue, MinValue, Mean, SD, Var
 
